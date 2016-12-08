bfefce62fc56131d2d6f970e73323b3112a729b4
Anton Ashmarin <aashmarin@gmail.com>
2016-12-08 12:41:47 +0300

    Base interfaces

---
+++ PolarTransfer/Core/ISportTrackerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarTransfer.Core
+{
+    interface ISportTrackerManager
+    {
+        CookieCollection Login();
+        string GetTrainingFileTcx();
+        bool AddTrainingResult();
+        bool AddTrainingTarget();
+        bool UpdateTrainingData();
+        TrainingData GetTrainingData();
+    }
+}
---
+++ PolarTransfer/Core/TrainingData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarTransfer.Core
+{
+    class TrainingData
+    {
+        string Description { get; set; }
+    }
+}
